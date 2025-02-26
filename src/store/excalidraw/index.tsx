55626c65515eb16a2e505d1bec9095a184f52531
robert <ext-robert.dabagov@kuper.ru>
2025-02-26 23:01:25 +0300

    ExcalidrawContext

---
+++ src/store/excalidraw/index.tsx
@@ -0,0 +1,22 @@
+import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
+import { createContext, useContext } from "react";
+
+const ExcalidrawContext = createContext<{
+  excalidrawAPI: ExcalidrawImperativeAPI | null;
+  setExcalidrawAPI: React.Dispatch<
+    React.SetStateAction<ExcalidrawImperativeAPI | null>
+  >;
+} | null>(null);
+
+export const useExcalidrawContext = () => {
+  const context = useContext(ExcalidrawContext);
+
+  if (!context)
+    throw new Error(
+      "Use ExcalidrawContext* component outside inner ExcalidrawContext"
+    );
+
+  return context;
+};
+
+export default ExcalidrawContext;
