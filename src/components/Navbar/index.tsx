900f73c73edc803afccbe770029da09c8541b371
robert <ext-robert.dabagov@kuper.ru>
2025-02-26 23:11:23 +0300

    useExport

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,6 @@
 import { Theme } from "@excalidraw/excalidraw/types/element/types";
 import { useSettingsContext } from "../../store/settings";
-import { useExcalidrawContext } from "../../store/excalidraw";
-import {
-  exportToBlob,
-  exportToCanvas,
-  exportToSvg,
-} from "@excalidraw/excalidraw";
-import { jsPDF } from "jspdf";
-import { convertPngBlobToPdf } from "../../utils/blob";
-import initialData from "../Editor/initialData";
+import { useExport } from "./hooks/useExport";
 
 export function Navbar() {
   const {
@@ -16,82 +8,8 @@ export function Navbar() {
     settings: { viewMode, zenMode, gridMode, theme },
   } = useSettingsContext();
 
-  const handleExportPDF = async () => {
-    if (excalidrawAPI) {
-      try {
-        if (!excalidrawAPI) {
-          return;
-        }
-        const canvas = await exportToCanvas({
-          elements: excalidrawAPI.getSceneElements(),
-          appState: {
-            ...initialData.appState,
-          },
-          files: excalidrawAPI.getFiles(),
-        });
-        const ctx = canvas.getContext("2d")!;
-        ctx.font = "30px Virgil";
-        ctx.strokeText("My custom text", 50, 60);
-
-        const imageData = canvas.toDataURL("image/png");
-
-        const pdf = new jsPDF();
-
-        pdf.addImage(imageData, "PNG", 10, 10, 180, 160);
-
-        pdf.save("excalidraw-export.pdf");
-      } catch (error) {
-        console.error("Ошибка при экспорте в PDF:", error);
-      }
-    }
-  };
-
-  const handleExportToSVG = async () => {
-    if (!excalidrawAPI) {
-      return;
-    }
-    const svg = await exportToSvg({
-      elements: excalidrawAPI?.getSceneElements(),
-      appState: {
-        ...initialData.appState,
-        width: 300,
-        height: 100,
-      },
-      files: excalidrawAPI?.getFiles(),
-    });
-
-    const blob = new Blob([svg.outerHTML], {
-      type: "image/svg+xml",
-    });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "excalidraw-export.svg";
-    document.body.appendChild(a);
-    a.click();
-
-    URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-  };
-
-  const { excalidrawAPI } = useExcalidrawContext();
-
-  const handleExportToBlob = async () => {
-    if (!excalidrawAPI) {
-      return;
-    }
-    const blob = await exportToBlob({
-      elements: excalidrawAPI?.getSceneElements(),
-      mimeType: "image/png",
-      appState: {
-        ...initialData.appState,
-      },
-      files: excalidrawAPI?.getFiles(),
-    });
-    // setBlobUrl(window.URL.createObjectURL(blob));
-    convertPngBlobToPdf(blob);
-  };
+  const { handleExportToBlob, handleExportPDF, handleExportToSVG } =
+    useExport();
 
   return (
     <div className='flex justify-between p-5 bg-indigo-300 items-center'>
