76059470aafcd67504cfab590f9e023f8ed822a7
robertd2000 <zdabaghov@mail.ru>
2025-03-07 13:58:29 +0300

    clean

--- src/components/Editor/hooks/useEditor.tsx
+++ src/components/Editor/hooks/useEditor.tsx
@@ -1,83 +1,84 @@
-import { useCallback, useEffect, useRef } from "react";
+import React, {useCallback, useEffect, useRef} from "react";
 
 import {
-  BinaryFileData,
-  ExcalidrawInitialDataState,
+    BinaryFileData,
+    ExcalidrawInitialDataState,
 } from "@excalidraw/excalidraw/types/types";
-import { MIME_TYPES, useHandleLibrary } from "@excalidraw/excalidraw";
-import { NonDeletedExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
-import { ResolvablePromise } from "@excalidraw/excalidraw/types/utils";
-import { useExcalidrawContext } from "../../../store/excalidraw";
-import { resolvablePromise } from "../../../utils";
+import {MIME_TYPES, useHandleLibrary} from "@excalidraw/excalidraw";
+import {NonDeletedExcalidrawElement} from "@excalidraw/excalidraw/types/element/types";
+import {ResolvablePromise} from "@excalidraw/excalidraw/types/utils";
+import {useExcalidrawContext} from "../../../store/excalidraw";
+import {resolvablePromise} from "../../../utils";
 import initialData from "../../../constants/initialData";
 
 export function useEditor() {
-  const appRef = useRef<any>(null);
+    const appRef = useRef<React.Ref<HTMLDivElement>>(null);
 
-  const initialStatePromiseRef = useRef<{
-    promise: ResolvablePromise<ExcalidrawInitialDataState | null>;
-  }>({ promise: null! });
-  if (!initialStatePromiseRef.current.promise) {
-    initialStatePromiseRef.current.promise = resolvablePromise();
-  }
-
-  const { excalidrawAPI, setExcalidrawAPI } = useExcalidrawContext();
-
-  useHandleLibrary({ excalidrawAPI });
-
-  useEffect(() => {
-    if (!excalidrawAPI) {
-      return;
+    const initialStatePromiseRef = useRef<{
+        promise: ResolvablePromise<ExcalidrawInitialDataState | null>;
+    }>({promise: null!});
+    if (!initialStatePromiseRef.current.promise) {
+        initialStatePromiseRef.current.promise = resolvablePromise();
     }
-    const fetchData = async () => {
-      const res = await fetch("/rocket.jpeg");
-      const imageData = await res.blob();
-      const reader = new FileReader();
-      reader.readAsDataURL(imageData);
 
-      reader.onload = function () {
-        const imagesArray: BinaryFileData[] = [
-          {
-            id: "rocket" as BinaryFileData["id"],
-            dataURL: reader.result as BinaryFileData["dataURL"],
-            mimeType: MIME_TYPES.jpg,
-            created: 1644915140367,
-            lastRetrieved: 1644915140367,
-          },
-        ];
-        //@ts-ignore
-        initialStatePromiseRef.current.promise.resolve(initialData);
-        excalidrawAPI.addFiles(imagesArray);
-      };
+    const {excalidrawAPI, setExcalidrawAPI} = useExcalidrawContext();
+
+    useHandleLibrary({excalidrawAPI});
+
+    useEffect(() => {
+        if (!excalidrawAPI) {
+            return;
+        }
+        const fetchData = async () => {
+            const res = await fetch("/rocket.jpeg");
+            const imageData = await res.blob();
+            const reader = new FileReader();
+            reader.readAsDataURL(imageData);
+
+            reader.onload = function () {
+                const imagesArray: BinaryFileData[] = [
+                    {
+                        id: "rocket" as BinaryFileData["id"],
+                        dataURL: reader.result as BinaryFileData["dataURL"],
+                        mimeType: MIME_TYPES.jpg,
+                        created: 1644915140367,
+                        lastRetrieved: 1644915140367,
+                    },
+                ];
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                //@ts-expect-error
+                initialStatePromiseRef.current.promise.resolve(initialData);
+                excalidrawAPI.addFiles(imagesArray);
+            };
+        };
+        fetchData()
+    }, [excalidrawAPI]);
+
+    const onLinkOpen = useCallback(
+        (
+            element: NonDeletedExcalidrawElement,
+            event: CustomEvent<{
+                nativeEvent: MouseEvent | React.PointerEvent<HTMLCanvasElement>;
+            }>
+        ) => {
+            const link = element.link!;
+            const {nativeEvent} = event.detail;
+            const isNewTab = nativeEvent.ctrlKey || nativeEvent.metaKey;
+            const isNewWindow = nativeEvent.shiftKey;
+            const isInternalLink =
+                link.startsWith("/") || link.includes(window.location.origin);
+            if (isInternalLink && !isNewTab && !isNewWindow) {
+                event.preventDefault();
+            }
+        },
+        []
+    );
+
+    return {
+        appRef,
+        initialStatePromiseRef,
+        excalidrawAPI,
+        setExcalidrawAPI,
+        onLinkOpen,
     };
-    fetchData();
-  }, [excalidrawAPI]);
-
-  const onLinkOpen = useCallback(
-    (
-      element: NonDeletedExcalidrawElement,
-      event: CustomEvent<{
-        nativeEvent: MouseEvent | React.PointerEvent<HTMLCanvasElement>;
-      }>
-    ) => {
-      const link = element.link!;
-      const { nativeEvent } = event.detail;
-      const isNewTab = nativeEvent.ctrlKey || nativeEvent.metaKey;
-      const isNewWindow = nativeEvent.shiftKey;
-      const isInternalLink =
-        link.startsWith("/") || link.includes(window.location.origin);
-      if (isInternalLink && !isNewTab && !isNewWindow) {
-        event.preventDefault();
-      }
-    },
-    []
-  );
-
-  return {
-    appRef,
-    initialStatePromiseRef,
-    excalidrawAPI,
-    setExcalidrawAPI,
-    onLinkOpen,
-  };
 }
